f8caebc06facdb78b5504e04fe58279a12f48938
HyeokSeong Kwon <khs50851@naver.com>
2021-11-01 14:10:45 +0900

    채팅 기능 구현

--- src/main/java/com/hy/chatapp/ChatController.java
+++ src/main/java/com/hy/chatapp/ChatController.java
@@ -1,8 +1,18 @@
 package com.hy.chatapp;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RequiredArgsConstructor
 @RestController
@@ -10,8 +20,17 @@ public class ChatController {
 	
 	private final ChatRepository chatRepository;
 	
+	@GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces = MediaType.TEXT_EVENT_STREAM_VALUE) // TEXT_EVENT_STREAM_VALUE이게 sse 프로토콜인데 이걸로 flux로 계속 데이터 흘려보낼수있음
+	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+		return chatRepository.mFindBySender(sender, receiver)
+				.subscribeOn(Schedulers.boundedElastic());
+	}
 	
-	
+	@PostMapping("/chat")
+	public Mono<Chat> setMsg(@RequestBody Chat chat){ // Mono는 데이터 한번만 리턴
+		chat.setCreatedAt(LocalDateTime.now());
+		return chatRepository.save(chat);
+	}
 	
 	
 }
--- src/main/java/com/hy/chatapp/ChatRepository.java
+++ src/main/java/com/hy/chatapp/ChatRepository.java
@@ -2,11 +2,14 @@ package com.hy.chatapp;
 
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import reactor.core.publisher.Flux;
 
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String>{
 	
+	@Tailable // 커서를 안닫고 계속 유지(db에서 찾아도 계속 유지됨)
 	@Query("{sender: ?0, receiver: ?1}")
 	Flux<Chat> mFindBySender(String sender,String receiver); // Flux(흐름) response를 유지하면서 데이터 흘려보내기
 	
