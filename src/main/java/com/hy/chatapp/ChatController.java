4c1874144952a0dfb4b461efc991f44716aa36cb
HyeokSeong Kwon <khs50851@naver.com>
2021-11-02 14:09:00 +0900

    구현 완료

--- src/main/java/com/hy/chatapp/ChatController.java
+++ src/main/java/com/hy/chatapp/ChatController.java
@@ -3,6 +3,7 @@ package com.hy.chatapp;
 import java.time.LocalDateTime;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +21,24 @@ public class ChatController {
 	
 	private final ChatRepository chatRepository;
 	
+	
+	/// 귓속말할때
+	@CrossOrigin
 	@GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces = MediaType.TEXT_EVENT_STREAM_VALUE) // TEXT_EVENT_STREAM_VALUE이게 sse 프로토콜인데 이걸로 flux로 계속 데이터 흘려보낼수있음
 	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
 		return chatRepository.mFindBySender(sender, receiver)
+					.subscribeOn(Schedulers.boundedElastic());
+	}
+	
+
+	@CrossOrigin
+	@GetMapping(value = "/chat/roomNum/{roomNum}",produces = MediaType.TEXT_EVENT_STREAM_VALUE) // TEXT_EVENT_STREAM_VALUE이게 sse 프로토콜인데 이걸로 flux로 계속 데이터 흘려보낼수있음
+	public Flux<Chat> findByroomNum(@PathVariable Integer roomNum) {
+		return chatRepository.mFindByRoomNum(roomNum)
 				.subscribeOn(Schedulers.boundedElastic());
 	}
 	
+	@CrossOrigin
 	@PostMapping("/chat")
 	public Mono<Chat> setMsg(@RequestBody Chat chat){ // Mono는 데이터 한번만 리턴
 		chat.setCreatedAt(LocalDateTime.now());
